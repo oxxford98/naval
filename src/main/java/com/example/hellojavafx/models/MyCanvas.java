2c71289572d4d193a72ffffdeda1ae68abac8ea5
Juan David Sevilla <sevilla.juan@correounivalle.edu.co>
2024-11-15 00:55:28 -0500

    feat: create board, humanplayer, player, robot player model

---
+++ src/main/java/com/example/hellojavafx/models/MyCanvas.java
@@ -0,0 +1,11 @@
+package com.example.hellojavafx.models;
+
+public class MyCanvas {
+    int[] coordinates;
+    int orientation;
+
+    MyCanvas(int[] coordinates, int orientation){
+        this.coordinates = coordinates;
+        this.orientation = orientation;
+    }
+}
